317a8b8faa37f2d07c3c770522810bfc286d62d6
Armando Salazar <armandosalazar351@gmail.com>
2023-12-04 22:00:08 -0600

    fix: warning unused method

--- src/main/java/org/armandosalazar/Animation.java
+++ src/main/java/org/armandosalazar/Animation.java
@@ -118,41 +118,41 @@ public abstract class Animation {
         }
     }
 
-    private static void bombermanStanding(int x, int y) {
-        // Manos
-        Graphics.setColorRGB(255, 255, 255);
-        Graphics.fillRect(x - 2, y + 14, 4, 4);
-        Graphics.fillRect(x - 4, y + 12, 4, 4);
-        Graphics.setColorRGB(255, 129, 111);
-        Graphics.fillRect(x - 6, y + 10, 4, 4);
-        Graphics.setColorRGB(255, 255, 255);
-        Graphics.fillRect(x + 10, y + 14, 4, 4);
-        Graphics.fillRect(x + 12, y + 12, 4, 4);
-        Graphics.setColorRGB(255, 129, 111);
-        Graphics.fillRect(x + 14, y + 10, 4, 4);
-        // Traje
-        Graphics.setColorRGB(100, 176, 255);
-        Graphics.fillRect(x, y + 6, 12, 10);
-        Graphics.fillRect(x + 2, y + 16, 8, 4);
-        // Cabeza
-        Graphics.setColorRGB(255, 255, 255);
-        Graphics.fillRect(x, y + 20, 12, 12);
-        Graphics.fillRect(x - 2, y + 22, 16, 8);
-        // Ojos
-        Graphics.setColorRGB(255, 129, 111);
-        Graphics.fillRect(x, y + 24, 12, 4);
-        Graphics.setColorRGB(56, 135, 0);
-        Graphics.fillRect(x + 2, y + 24, 2, 4);
-        Graphics.fillRect(x + 8, y + 24, 2, 4);
-        // Pans
-        Graphics.setColorRGB(255, 255, 255);
-        Graphics.fillRect(x, y + 4, 4, 4);
-        Graphics.fillRect(x + 8, y + 4, 4, 4);
-        // Pies
-        Graphics.setColorRGB(255, 129, 111);
-        Graphics.fillRect(x, y, 4, 4);
-        Graphics.fillRect(x + 8, y, 4, 4);
-    }
+    // private static void bombermanStanding(int x, int y) {
+    //    // Manos
+    //     Graphics.setColorRGB(255, 255, 255);
+    //     Graphics.fillRect(x - 2, y + 14, 4, 4);
+    //     Graphics.fillRect(x - 4, y + 12, 4, 4);
+    //     Graphics.setColorRGB(255, 129, 111);
+    //     Graphics.fillRect(x - 6, y + 10, 4, 4);
+    //     Graphics.setColorRGB(255, 255, 255);
+    //     Graphics.fillRect(x + 10, y + 14, 4, 4);
+    //     Graphics.fillRect(x + 12, y + 12, 4, 4);
+    //     Graphics.setColorRGB(255, 129, 111);
+    //     Graphics.fillRect(x + 14, y + 10, 4, 4);
+    //     // Traje
+    //     Graphics.setColorRGB(100, 176, 255);
+    //     Graphics.fillRect(x, y + 6, 12, 10);
+    //     Graphics.fillRect(x + 2, y + 16, 8, 4);
+    //     // Cabeza
+    //     Graphics.setColorRGB(255, 255, 255);
+    //     Graphics.fillRect(x, y + 20, 12, 12);
+    //     Graphics.fillRect(x - 2, y + 22, 16, 8);
+    //     // Ojos
+    //     Graphics.setColorRGB(255, 129, 111);
+    //     Graphics.fillRect(x, y + 24, 12, 4);
+    //     Graphics.setColorRGB(56, 135, 0);
+    //     Graphics.fillRect(x + 2, y + 24, 2, 4);
+    //     Graphics.fillRect(x + 8, y + 24, 2, 4);
+    //     // Pans
+    //     Graphics.setColorRGB(255, 255, 255);
+    //     Graphics.fillRect(x, y + 4, 4, 4);
+    //     Graphics.fillRect(x + 8, y + 4, 4, 4);
+    //     // Pies
+    //     Graphics.setColorRGB(255, 129, 111);
+    //     Graphics.fillRect(x, y, 4, 4);
+    //     Graphics.fillRect(x + 8, y, 4, 4);
+    // }
 
     private static void bombermanRightFoot(int x, int y) {
         // Manos
